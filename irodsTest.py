0726d782ebc48bbea753b7c4138b10b4914a6270
Ramen-Noodle <clee9g0@gmail.com>
2024-06-20 14:16:56 -0400

    Implemented authentication and create, remove, and stat for collections.
    Added collections.list()
    
    Finished basic implementation of collections functions
    
    Figured out reading dictionary response from API
    
    Added comments, cleaned up irodsManager
    
    Began unit tests for collections, started standardizing returns
    
    Standardized return data across collections functions
    
    Revised return values
    
    Set up new authentication flow with collections
    
    Preliminary unit testing for collections operations

--- irodsTest.py
+++ irodsTest.py
@@ -3,8 +3,34 @@ import irodsManager
 #manager = irodsManager.collections_manager('rods', 'rods', 'http://localhost:9001/irods-http-api/', '0.3.0')
 #print(manager.stat('/tempZone/home/rods'))
 
-m = irodsManager.manager('rods', 'rods', 'http://localhost:9001/irods-http-api/', '0.3.0')
-print(m.collections.stat('/tempZone/home/rods'))
-#print(m.collections.remove('/tempZone/home/new', 0, 0))
-#print(m.collections.stat('/tempZone/home/new'))
-print(m.collections.create('/tempZone/home/new', 0))
+api = irodsManager.manager('http://localhost:9001/irods-http-api/0.3.0')
+token = api.authenticate('rods', 'rods')
+api.setToken(token)
+
+# params = {
+#     'jebediah': 'null',
+#     'scott': 'read'
+# }
+
+ops_permissions = [
+    {
+        'entity_name': 'jebediah',
+        'acl': 'read'
+    },
+    {
+        'entity_name': 'scott',
+        'acl': 'read'
+    }
+]
+
+ops_metadata = [
+    {
+        'operation': 'add',
+        'attribute': 'eyeballs',
+        'value': 'itchy'
+    }
+]
+
+print(api.collections.stat('/tempZone/home/rods'))
+
+
