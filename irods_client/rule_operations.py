73613f6aead8184925b428ee882d0366dae722ab
cdlee3 <cdlee3@ncsu.edu>
2024-07-31 13:10:45 -0400

    [#20] Replaced documentation comments with docstrings

--- irods_client/rule_operations.py
+++ irods_client/rule_operations.py
@@ -2,16 +2,24 @@ import requests
 import json
 
 class Rules:
-    # Initializes rules_manager with variables from the parent class.
+    
     def __init__(self, url_base: str):
+        """" 
+        Initializes Rules with a base url. 
+        Token is set to None initially, and updated when setToken() is called in irodsClient.
+        """
         self.url_base = url_base
         self.token = None
 
-    # Lists available rule engine plugin instances.
-    # return
-    # - Status code 2XX: List of rule engine plugins.
-    # - Other: Status code and return message.
+
     def list_rule_engines(self):
+        """
+        Lists available rule engine plugin instances.
+
+        Returns
+        - A dict containing the HTTP status code and iRODS response.
+        - The iRODS response is only valid if no error occurred during HTTP communication.
+        """
         
         headers = {
             'Authorization': 'Bearer ' + self.token,
@@ -53,13 +61,18 @@ class Rules:
             )
 
 
-    # Executes rule code.
-    # params
-    # - rule_text: The rule code to execute.
-    # - rep_instance (optional): The rule engine plugin to run the rule-text against.
-    # returns
-    # - Status code and response message.
     def execute(self, rule_text: str, rep_instance: str=''):
+        """
+        Executes rule code.
+        
+        Parameters
+        - rule_text: The rule code to execute.
+        - rep_instance (optional): The rule engine plugin to run the rule-text against.
+
+        Returns
+        - A dict containing the HTTP status code and iRODS response.
+        - The iRODS response is only valid if no error occurred during HTTP communication.
+        """
         if (self.token == None):
             raise RuntimeError('No token set. Use setToken() to set the auth token to be used')
         if (not isinstance(rule_text, str)):
@@ -111,19 +124,24 @@ class Rules:
                 }
             )
         
-    
-    # Removes a delay rule from the catalog.
-    # params
-    # - rule_id: The id of the delay rule to be removed.
-    # returns
-    # - Status code and response message.
+
     def remove_delay_rule(self, rule_id: int):
+        """
+        Removes a delay rule from the catalog.
+        
+        Parameters
+        - rule_id: The id of the delay rule to be removed.
+
+        Returns
+        - A dict containing the HTTP status code and iRODS response.
+        - The iRODS response is only valid if no error occurred during HTTP communication.
+        """
         if (self.token == None):
             raise RuntimeError('No token set. Use setToken() to set the auth token to be used')
         if (not isinstance(rule_id, int)):
             raise TypeError('rule_id must be an int')
-        if (not rule_id > 0):
-            raise ValueError('rule_id must be greater than 0')
+        if (not rule_id >= 0):
+            raise ValueError('rule_id must be greater than or equal to 0')
 
         headers = {
             'Authorization': 'Bearer ' + self.token,
--- irods_client/ticket_operations.py
+++ irods_client/ticket_operations.py
@@ -2,26 +2,36 @@ import requests
 import json
 
 class Tickets:
-    # Initializes ticket_manager with variables from the parent class.
+
     def __init__(self, url_base: str):
+        """" 
+        Initializes Tickets with a base url. 
+        Token is set to None initially, and updated when setToken() is called in irodsClient.
+        """
         self.url_base = url_base
         self.token = None
 
-    # Creates a new ticket for a collection or data object.
-    # params
-    # - lpath: Absolute logical path to a data object or collection.
-    # - type (optional): Read or write. Defaults to read.
-    # - use_count (optional): Number of times the ticket can be used.
-    # - write_data_object_count (optional): Max number of writes that can be performed.
-    # - write_byte_count (optional): Max number of bytes that can be written.
-    # - seconds_until_expiration (optional): Number of seconds before the ticket expires.
-    # - users (optional): Comma-delimited list of users allowed to use the ticket
-    # - groups (optional): Comma-delimited list of groups allowed to use the ticket
-    # - hosts (optional): Comma-delimited list of hosts allowed to use the ticket
-    # returns
-    # - Status code and response message.
+
     def create(self, lpath: str, type: str='read', use_count: int=-1, write_data_object_count: int=-1, write_byte_count: int=-1,
                 seconds_until_expiration: int=-1, users: str='', groups: str='', hosts: str=''):
+        """
+        Creates a new ticket for a collection or data object.
+        
+        Parameters
+        - lpath: Absolute logical path to a data object or collection.
+        - type (optional): Read or write. Defaults to read.
+        - use_count (optional): Number of times the ticket can be used.
+        - write_data_object_count (optional): Max number of writes that can be performed.
+        - write_byte_count (optional): Max number of bytes that can be written.
+        - seconds_until_expiration (optional): Number of seconds before the ticket expires.
+        - users (optional): Comma-delimited list of users allowed to use the ticket.
+        - groups (optional): Comma-delimited list of groups allowed to use the ticket.
+        - hosts (optional): Comma-delimited list of hosts allowed to use the ticket.
+
+        Returns
+        - A dict containing the HTTP status code and iRODS response.
+        - The iRODS response is only valid if no error occurred during HTTP communication.
+        """
         if (self.token == None):
             raise RuntimeError('No token set. Use setToken() to set the auth token to be used')
         if (not isinstance(lpath, str)):
@@ -32,20 +42,20 @@ class Tickets:
             raise ValueError('type must be either read or write')
         if (not isinstance(use_count, int)):
             raise TypeError('use_count must be an int')
-        if (not use_count > -1):
-            raise ValueError('use_count must be greater than 0 or flag value -1')
+        if (not use_count >= -1):
+            raise ValueError('use_count must be greater than or equal to 0 or flag value -1')
         if (not isinstance(write_data_object_count, int)):
             raise TypeError('write_data_object_count must be an int')
-        if (not write_data_object_count > -1):
-            raise ValueError('write_data_object_count must be greater than 0 or flag value -1')
+        if (not write_data_object_count >= -1):
+            raise ValueError('write_data_object_count must be greater than or equal to 0 or flag value -1')
         if (not isinstance(write_byte_count, int)):
             raise TypeError('write_byte_count must be an int')
-        if (not write_byte_count > -1):
-            raise ValueError('write_byte_count must be greater than 0 or flag value -1')
+        if (not write_byte_count >= -1):
+            raise ValueError('write_byte_count must be greater than or equal to 0 or flag value -1')
         if (not isinstance(seconds_until_expiration, int)):
             raise TypeError('seconds_until_expiration must be an int')
-        if (not seconds_until_expiration > -1):
-            raise ValueError('seconds_until_expiration must be greater than 0 or flag value -1')
+        if (not seconds_until_expiration >= -1):
+            raise ValueError('seconds_until_expiration must be greater than or equal to 0 or flag value -1')
         if (not isinstance(users, str)):
             raise TypeError('users must be an string')
         if (not isinstance(groups, str)):
@@ -110,12 +120,17 @@ class Tickets:
             )
         
 
-    # Removes an existing ticket.
-    # params
-    # - name: The ticket to be removed.
-    # returns
-    # - Status code and response message.
     def remove(self, name: str):
+        """
+        Removes an existing ticket.
+        
+        Parameters
+        - name: The ticket to be removed.
+
+        Returns
+        - A dict containing the HTTP status code and iRODS response.
+        - The iRODS response is only valid if no error occurred during HTTP communication.
+        """
         if (self.token == None):
             raise RuntimeError('No token set. Use setToken() to set the auth token to be used')
         if (not isinstance(name, str)):
